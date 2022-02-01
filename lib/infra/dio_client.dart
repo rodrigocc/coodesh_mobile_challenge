26454c53c86ab0e31dc374fc56f6ec4dac10571a
rodrigo <rodrigoasilvacc@gmail.com>
2022-01-31 22:58:28 -0300

    Feat: Implements a infra layer

---
+++ lib/infra/dio_client.dart
@@ -0,0 +1,9 @@
+import 'package:coodesh_mobile_challenge/infra/http_service_interface.dart';
+
+class DioClient implements IHttpService {
+  @override
+  Future get(String url) {
+    // TODO: implement get
+    throw UnimplementedError();
+  }
+}
