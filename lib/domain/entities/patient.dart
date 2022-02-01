8ed9ea8ebad8d48aed195997384956d362246005
rodrigo <rodrigoasilvacc@gmail.com>
2022-01-31 23:30:57 -0300

    Feat :Create Patient Entitie and UseCase Layer

--- lib/domain/entities/patient.dart
+++ lib/domain/entities/patient.dart
@@ -1,1 +1,13 @@
-class Patient {}
+class Patient {
+  final String imgPicture;
+  final String name;
+  final String gender;
+  final String birthidayDate;
+  final String email;
+  final String nationality;
+  final String adress;
+  final String id;
+
+  const Patient(this.birthidayDate, this.gender, this.imgPicture, this.name,
+      this.nationality, this.adress, this.email, this.id);
+}
